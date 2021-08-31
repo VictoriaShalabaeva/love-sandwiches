7a0d3dea635fe9545410a35e7e8dcefc488128d1
VictoriaShalabaeva <shalabaevavt@gmail.com>
2021-08-31 20:51:11 +0000

    Wire up APIs and confirm access to google sheet

---
+++ run.py
@@ -0,0 +1,22 @@
+# At the beginning put in terminal: 'pip3 install gspread google-auth'
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [ # in Python we  write constant variable names in capitals
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
+
+print(data)
